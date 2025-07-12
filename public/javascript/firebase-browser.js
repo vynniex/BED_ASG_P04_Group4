985e0dc300db7cee6e68affa28483213730f4742
vynniex <s10270487@connect.np.edu.sg>
2025-07-12 22:55:27 +0800

    Added firebase for browser compatibility

---
+++ public/javascript/firebase-browser.js
@@ -0,0 +1,11 @@
+// Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdIA-5d3nFsd7meva1YUvQWSc3O2qgqo0",
+  authDomain: "bed-asg-840eb.firebaseapp.com",
+  projectId: "bed-asg-840eb",
+  storageBucket: "bed-asg-840eb.firebasestorage.app",
+  messagingSenderId: "970606110731",
+  appId: "1:970606110731:web:60e1534b88e0c37689ff0e"
+};
+
+firebase.initializeApp(firebaseConfig);
