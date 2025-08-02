833606336b515c721bc273f669ff9db2bbba647b
gracechua93 <chua.faithboy@gmail.com>
2025-08-02 12:52:22 +0800

    fix unit testing

--- tests/userController.test.js
+++ tests/userController.test.js
@@ -3,35 +3,22 @@ const userController = require("../appointments-api-grace/controllers/userContro
 const apptsModel = require("../appointments-api-grace/models/apptsModel");
 const userModel = require("../appointments-api-grace/models/userModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // Mock the User model
 jest.mock("../appointments-api-grace/models/userModel");
 jest.mock("../appointments-api-grace/models/apptsModel");
-jest.mock("bcryptjs", () => ({
-    compare: jest.fn().mockResolvedValue(true),
-    hash: jest.fn().mockResolvedValue('hashedPassword'), 
-    genSalt: jest.fn().mockResolvedValue('salt'),
-}));
 
 // mock the Express response object
 const mockResponse = () => {
     const res = {};
-    res.status = jest.fn().mockReturnValue(res);
-    res.json = jest.fn().mockResolvedValue(res);
+    res.status = jest.fn().mockReturnThis();
+    res.json = jest.fn();
     res.send = jest.fn().mockResolvedValue(res);
     return res;
 }
 
-const mockUser = {
-    nric: "S5056789C",
-    userId: 1,
-    full_name: "Bob Tan",
-    email: "bob@gmail.com",
-    contact_num: "90011234",
-    dob: "1950-02-28",
-    password: "hashedPassword",
-    // await bcrypt.hash("Bob123", 10)
-};
+;
 
 describe("userController", () => {
     beforeEach(() => {
@@ -61,12 +48,22 @@ describe("userController", () => {
     });
 
     it("loginUser should login a user and return token", async () => {
+        const mockUser = {
+            nric: "S5056789C",
+            userId: 1,
+            full_name: "Bob Tan",
+            email: "bob@gmail.com",
+            contact_num: "90011234",
+            dob: "1950-02-28",
+            password: await bcrypt.hash("Bob123", 10)
+        }
         const req = {
             body: {
                 email: "bob@gmail.com",
-                password: "bob123"
+                password: "Bob123"
             }
         };
+
         const res = mockResponse();
 
         userModel.findUserByEmail.mockResolvedValue(mockUser);
@@ -74,26 +71,36 @@ describe("userController", () => {
         await userController.loginUser(req, res);
 
         expect(res.status).toHaveBeenCalledWith(200);
-        expect(res.json).toHaveBeenCalledWith(expect.objectContaining({ token: expect.any(String) }));
+        const jsonResponse = res.json.mock.calls[0][0];
+
+        expect(jsonResponse).toHaveProperty('message', 'Login successful');
+        expect(jsonResponse).toHaveProperty('token');
+        expect(jsonResponse.user).toEqual({
+            fullName: mockUser.full_name,
+            email: mockUser.email,
+            contact: mockUser.contact_num,
+            dob: mockUser.dob
+        });
     });
 
-    // it("verify should verify NRIC successfully", async () => {
-    //     const hashedNric = await bcrypt.hash("S5056789C", 10);
-    //     const req = {
-    //         body: { nric: "S5056789C" },
-    //         headers: { authorization: `Bearer ${token}` }
-    //     };
-    //     const res = mockResponse();
+    it("verify should verify NRIC successfully", async () => {
+        const hashedNric = await bcrypt.hash("S5056789C", 10);
+        const req = {
+            body: { nric: "S5056789C" },
+            headers: { authorization: `Bearer testToken` }
+        };
+        const res = mockResponse();
 
-    //     const user = { userId: 1, nric_fin: hashedNric };
-    //     jwt.verify = jest.fn().mockReturnValue({ id: 1 });
-    //     userModel.findUserById.mockResolvedValue(user);
+        const user = { userId: 1, nric_fin: hashedNric };
+        
+        jwt.verify = jest.fn().mockReturnValue({ id: 1 });
+        userModel.findUserById.mockResolvedValue(user);
 
-    //     await userController.verify(req, res);
+        await userController.verify(req, res);
 
-    //     expect(res.status).toHaveBeenCalledWith(200);
-    //     expect(res.json).toHaveBeenCalledWith({ message: "NRIC verification successful." });
-    // });
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({ message: "NRIC verification successful." });
+    });
 
     it("getUserDetailsById should return user details", async () => {
         const req = { user: { id: "1" } };
